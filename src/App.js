f53c160a27bc799f288ac75841bd580ab59fd31e
Aniket Tiwari <aniket.76907@gmail.com>
2023-10-13 23:03:01 +0530

    Data Transfer learning
    - Learnt how to send data from nodeJs server to ReactJs app
    - Learnt difference between reactJs server and node server
    - Currently we can send data from nodeServer to react app using /getData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Axios from "axios";
 
 function App(){
+
+  // getting data from backend to show on front end
+  const [data, setData] = useState();
+
+  const getData = async() => {
+    const response = await Axios.get("http://localhost:5000/getData");
+    setData(response.data);
+  }
+
+  console.log("The value of data passed is: " + data);
+
+  useEffect(() => {
+    getData()
+  }, []);
+
   return(
-    <div>To all those out there</div>
+    <div>To all those out there {data}</div>
   );
 }
 
-export default App();
+export default App;
